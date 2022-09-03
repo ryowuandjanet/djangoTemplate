843026f05b926c1a6771cf282bc73ede22f8db5c
ryowuandjanet <ryowu0329@gmail.com>
2022-09-04 02:22:27 +0800

    add tailwindcss

--- src/settings/base.py
+++ src/settings/base.py
@@ -115,6 +115,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
