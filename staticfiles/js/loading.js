ef2ba075925d2a9b8857fee022c80745fc6ad49c
ryowuandjanet <ryowu0329@gmail.com>
2024-08-13 07:00:21 +0000

    CRUD bootstrap-2

--- staticfiles/js/loading.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const form = document.querySelector('form');
-    const spinner = document.getElementById('loading-spinner');
-
-    form.addEventListener('submit', function () {
-        spinner.classList.remove('hidden');
-    });
-});
